61afadc66d360651a4841026d72a586e78c39dc4
milddaa <milda94@gmail.com>
2018-02-19 16:22:47 +0100

    A bash script which returns a template project folder structure

---
+++ project_template.sh
@@ -0,0 +1,17 @@
+mkdir Project
+cd Project
+touch README.txt
+touch runall.sh
+# Make a directory where all the data sets are stored
+mkdir Datasets
+# Make a directory for all the results
+mkdir Results
+# Make a directory where the final code will be stored
+mkdir Source_code
+# Make a directory where all the intermediate scripts will be stored
+mkdir Bin
+# Make a directory where all the documents, instructions and diary is stored
+mkdir Documents
+cd Documents
+# Make a diary for taking notes every day during the project
+touch diary.txt
